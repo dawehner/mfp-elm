587dfeaecea487e25e5e8b8c1a88e97036b4b549
Daniel Wehner <dawehner@googlemail.com>
2020-05-16 21:37:33 +0100

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+          }
+        }
+      }]
+  },
+  devServer: {
+    inline: true,
+    stats: 'errors-only'
+  }
+};
